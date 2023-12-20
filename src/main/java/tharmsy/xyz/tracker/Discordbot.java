cc0ff7ad8cc807cedf7fbc7c16912513fc7bfb1f
TharmsyXD <133595791+TharmsyXD@users.noreply.github.com>
2023-12-20 21:49:34 +0000

    Plugin Base :fire:

---
+++ src/main/java/tharmsy/xyz/tracker/Discordbot.java
@@ -0,0 +1,17 @@
+package tharmsy.xyz.tracker;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Discordbot extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
